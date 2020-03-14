a58f8ce33185bc99c3e317104820d7846bb76a06
gfattinger <gernot.fattinger@ilutions.net>
2020-03-14 13:03:09 -0400

    Cleaned up config file.

--- WiHome_Config.h
+++ WiHome_Config.h
@@ -1,48 +1,5 @@
 #define SERIAL_DEBUG true
 
-// // Pin config Gosund wall switch V1
-// #define PIN_LED     13
-// #define PIN_BUTTON  0
-// #define PIN_RELAY   12
-// #define PIN_LED_ACTIVE_LOW true
-// #define PIN_RELAY_ACTIVE_LOW false
-
-// // Pin config Gosund wall switch SW1_V1.2
-// // #define PIN_LED     16 // RED
-// #define PIN_LED     1 // GREEN but also Serial TX
-// #define PIN_BUTTON  0
-// #define PIN_RELAY   14
-// #define PIN_LED_ACTIVE_LOW true
-// #define PIN_RELAY_ACTIVE_LOW false
-
-// // Pin config Sonoff Basic RF R2 POWER V1.0
-// #define PIN_LED     13
-// #define PIN_BUTTON  0
-// #define PIN_RELAY   12
-// #define PIN_LED_ACTIVE_LOW true
-// #define PIN_RELAY_ACTIVE_LOW false
-
-// // Pin config Sonoff Touch EU WIFI V1.0 (2016-8-3)
-// #define PIN_LED     13
-// #define PIN_BUTTON  0
-// #define PIN_RELAY   12
-// #define PIN_LED_ACTIVE_LOW true
-// #define PIN_RELAY_ACTIVE_LOW false
-
-// // Pin config Sonoff Basic R2 V1.0 (2017-10-11)
-// #define PIN_LED     13
-// #define PIN_BUTTON  0
-// #define PIN_RELAY   12
-// #define PIN_LED_ACTIVE_LOW true
-// #define PIN_RELAY_ACTIVE_LOW false
-
-// // Pin config Sonoff POW Ver 2.0
-// #define PIN_LED     15
-// #define PIN_BUTTON  0
-// #define PIN_RELAY   12
-// #define PIN_LED_ACTIVE_LOW false
-// #define PIN_RELAY_ACTIVE_LOW false
-
 // Pin config WiHome_Clock
 #define PIN_LED     13
 #define PIN_BUTTON  0
