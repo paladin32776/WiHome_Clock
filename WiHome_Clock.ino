3c182ff7830de2548de3fa4ef0f3f7af5e2f8023
Guenter Fattinger <guefat@aon.at>
2021-08-16 17:24:54 +0200

    NTP changes
    Changed NTPclient library to Arduino TimeLib.h for NTP.

--- WiHome_Clock.ino
+++ WiHome_Clock.ino
@@ -9,12 +9,9 @@
 #include "WiHome_Config.h"
 #include "EnoughTimePassed.h"
 
-#include <NTPClient.h>
+// #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include "TimeLib.h"
-
-//#include "HDC1080.h"
-//#include "CCS811.h"
 #include <Wire.h>
 #include "SparkFunBME280.h"
 #include "BH1750.h"
@@ -28,18 +25,18 @@ SignalLED led(PIN_LED,SLED_BLINK_FAST_1,PIN_LED_ACTIVE_LOW);
 NoBounceButtons nbb;
 char button;
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
+static const char ntpServerName[] = "at.pool.ntp.org";
+// NTPClient timeClient(ntpUDP);
+time_t getNtpTime();
+void sendNTPpacket(IPAddress &address);
 time_t t, t_last;
 
 // Object for display:
 MultiDisp7 m7(display_count, addr, type, subdigit);
 
 // Objects and variables for sensors:
-//HDC1080 hdc;
-//float T,RH;
-//CCS811 ccs;
-//unsigned int CO2, TVOC;
 BME280 bme;
 float T,RH,P,TK,PN,PNN;
 bool bme_ok = false;
@@ -81,10 +78,74 @@ void setup()
   m7.dim(100);
   m7.print((char*)"                    ");
   m7.print((char*)"boot");
+  
+  // Setup NTP callback and time interval for clock
+  Udp.begin(localPort);
+  setSyncProvider(getNtpTime);
+  setSyncInterval(5);
   // Set internal clock and timezone:
-  timeClient.begin();
+  // timeClient.begin();
   // timeClient.setTimeOffset(2*3600); // Mitteleuropäische Sommerzeit
-  t_last = timeClient.getEpochTime();
+  // t_last = timeClient.getEpochTime();
+}
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  Serial.print(ntpServerName);
+  Serial.print(": ");
+  Serial.println(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + TIMEZONE_OFFSET * SECS_PER_HOUR;
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
 }
 
 bool DST(time_t t)
@@ -101,7 +162,6 @@ bool DST(time_t t)
   return isDST;
 }
 
-
 void loop()
 {
   char s[20];
@@ -111,15 +171,13 @@ void loop()
   nbb.check();
   led.check();
 
-  if (whc.status()==1 && (timeset == false || etp_timeset.enough_time()))
-  {
-    Serial.printf("Getting time from NTP server.\n");
-    timeset = timeClient.update();
-  }
-  t = timeClient.getEpochTime();
+  t = now();
 
-  // Accouting for timezone and DST
-  t += TIMEZONE_OFFSET*3600;
+
+  if (timeStatus()==timeSet)
+    setSyncInterval(600);
+
+  // Accounting DST
   if (DST(t))
     t += 3600;
 
